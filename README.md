01bc8fec25333f89d43f2ab45a179ffbebc3e646
FloatingOctothorpe <git@floatingoctothorpe.uk>
2023-06-25 09:58:01 +0100

    Update to use API tokens
    Update script to use Cloudflare limited API tokens instead of API keys
    with full access.
    
    Other minor changes include:
    
     - Include time in log messages
     - Update requests library version
     - Update README.md with setup instructions

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # cloudflare-dynamic-dns
 Python script to use Cloudflare for dynamic DNS
+
+## Setup
+
+ 1. [Create an A
+    record](https://support.cloudflare.com/hc/en-us/articles/360019093151-Managing-DNS-records-in-Cloudflare#h_60566325041543261564371)
+    in CloudFlare
+
+ 2. [Create an API token](https://developers.cloudflare.com/api/tokens/create)
+    with write access to the zone the A record is in.
+
+ 3. Run `update.py` to generate a config file and update the A record to your
+    current public IP address:
+
+    ```
+    $ python update.py
+    Auth token: Njk2ZD-I5ZTA5NDBhNDk1Nzc0OGZlM2ZjOWVmZDI
+    Zone name: example.com
+    A record name: updateme.example.com
+    INFO:root:Saving new config to cloudflare-dynamic-dns.json
+    INFO:root:Updated updateme.example.com.uk (1.2.3.4)
+    ```
+
+ 4. Periodically re-run the script (e.g. via cron) to keep the A record
+    up-to-date.
--- update.py
+++ update.py
@@ -16,60 +16,74 @@ CONFIG_FILE = 'cloudflare-dynamic-dns.json'
 
 def get_public_ip():
     """Return your current public IP address"""
-    response = requests.get('https://api.ipify.org/')
+    response = requests.get('https://api.ipify.org/', timeout=15)
     response.raise_for_status()
     return response.text.strip()
 
-def update_record(key, email, record):
+def update_record(token, record):
     """Update a DNS record"""
-    url = 'https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s' \
-            % (record['zone_id'], record['id'])
-    response = requests.put(url, headers={'Content-Type': 'application/json',
-                                          'X-Auth-Email': email,
-                                          'X-Auth-Key': key},
-                            data=json.dumps(record))
+
+    response = requests.put(
+        f'https://api.cloudflare.com/client/v4/zones/{record["zone_id"]}/dns_records/{record["id"]}',
+        headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'},
+        data=json.dumps(record),
+        timeout=15
+    )
     response.raise_for_status()
     return response.json().get('success')
 
-def get_a_record_details(key, email, zone_id, name):
+def get_a_record_details(token, zone_id, name):
     """Get current DNS A record details"""
-    url = 'https://api.cloudflare.com/client/v4/zones/%s/dns_records/?name=%s&type=A' \
-            % (zone_id, name)
-    response = requests.get(url, headers={'Content-Type': 'application/json',
-                                          'X-Auth-Email': email,
-                                          'X-Auth-Key': key})
+
+    response = requests.get(
+        f'https://api.cloudflare.com/client/v4/zones/{zone_id}/dns_records/?name={name}&type=A',
+        headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'},
+        timeout=15
+    )
     response.raise_for_status()
     if response.status_code == 200 and response.json().get('success'):
         return response.json()['result'][0]
-    else:
-        return None
+    return None
 
-def get_zone_id(key, email, zone_name):
+def get_zone_id(token, zone_name):
     """Get the 32 character zone identifier for a given zone"""
-    url = 'https://api.cloudflare.com/client/v4/zones?name=%s' % zone_name
-    response = requests.get(url, headers={'Content-Type': 'application/json',
-                                          'X-Auth-Email': email,
-                                          'X-Auth-Key': key})
+
+    response = requests.get(
+        f'https://api.cloudflare.com/client/v4/zones?name={zone_name}',
+        headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'},
+        timeout=15
+    )
     response.raise_for_status()
     if response.status_code == 200 and response.json().get('success'):
         return response.json()['result'][0]['id']
-    else:
-        return None
+    return None
 
 def ask_for_config():
     """Prompt for configuration details"""
-    email = input('CloudFlare registered email: ')
-    key = input('Auth key: ')
+
+    token = input('Auth token: ')
     zone = input('Zone name: ')
+    try:
+        zone_id = get_zone_id(token, zone)
+    except requests.exceptions.HTTPError:
+        logging.error('Invalid token')
+        sys.exit(1)
+    except IndexError:
+        logging.error('Unable to find zone "%s"', zone)
+        sys.exit(2)
+
     record = input('A record name: ')
-    assert isinstance(email, str) and isinstance(key, str) \
-            and isinstance(zone, str) and isinstance(record, str)
-    return {'email': email, 'key': key, 'zone': zone, 'record': record}
+    try:
+        get_a_record_details(token, zone_id, record)
+    except IndexError:
+        logging.error('Invalid or missing A record "%s"', record)
+        sys.exit(3)
+    return {'token': token, 'zone': zone, 'record': record}
 
 def save_config(config, path):
     """Save config values to a file for future use"""
     try:
-        with open(path, 'w') as config_data:
+        with open(path, 'w', encoding='utf-8') as config_data:
             config_data.write(json.dumps(config, indent=2))
         # Note: file permissions will be ignored on windows
         os.chmod(path, stat.S_IRUSR | stat.S_IWUSR)
@@ -80,7 +94,7 @@ def get_config():
     """Load or prompt for configuration"""
     config_path = os.path.join(os.path.dirname(sys.argv[0]), CONFIG_FILE)
     try:
-        with open(config_path, 'r') as config_data:
+        with open(config_path, 'r', encoding='utf-8') as config_data:
             config = json.load(config_data)
     except IOError:
         logging.debug('Config file missing: %s', config_path)
@@ -91,9 +105,9 @@ def get_config():
 
 def main():
     """Update DNS A record based on current public IP address"""
+    config = get_config()
     public_ip = get_public_ip()
     logging.debug('Public ip is: %s', public_ip)
-    config = get_config()
     current_ip = socket.gethostbyname(config['record'])
     logging.debug('%s currently points to %s', config['record'], current_ip)
     if current_ip == public_ip:
@@ -101,29 +115,23 @@ def main():
         return
 
     logging.debug('Fetching Zone ID for %s', config['zone'])
-    zone_id = get_zone_id(config['key'], config['email'], config['zone'])
+    zone_id = get_zone_id(config['token'], config['zone'])
     logging.debug('Zone ID: %s', zone_id)
     logging.debug('Fetching record details for %s', config['record'])
-    record = get_a_record_details(config['key'], config['email'], zone_id, config['record'])
+    record = get_a_record_details(config['token'], zone_id, config['record'])
     logging.debug('Record ID: %s', record['id'])
     del record['created_on']
     del record['modified_on']
     record['content'] = public_ip
     logging.debug('Updating %s to point to %s', config['record'], public_ip)
-    if update_record(config['key'], config['email'], record):
+    if update_record(config['token'], record):
         logging.info('Updated %s (%s)', config['record'], public_ip)
     else:
         logging.warning('Failed to update %s', config['record'])
 
-# Treat input like raw_input in python 2.x
-try:
-    # pylint: disable=redefined-builtin, invalid-name
-    input = raw_input
-    # pylint: enable=redefined-builtin, invalid-name
-except NameError:
-    pass
-
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.basicConfig(
+            format='%(asctime)s %(levelname)s: %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            level=logging.INFO)
     main()
